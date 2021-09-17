657fa9637beb033d7905cdb75a5b6382e8f5ef5a
Fallen Starrr <juzopigeon@gmail.com>
2021-09-17 19:20:32 +0600

    changes

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -2,10 +2,24 @@ import axios from 'axios'
 
 
 const $host = axios.create({
-  baseUrl: ''
+  baseUrl: 'http://localhost:5000/'
 })
 
 
 const $authHost = axios.create({
+  baseUrl:  'http://localhost:5000/'
+}) 
+
+
+const authInterceptor = config => { 
   
-})
+  config.headear.authorization = `Bearer ${localStorage.getItem('token')}`
+  return config
+ }
+
+ $authHost.interceptors.request.use(authInterceptor)
+
+ export {
+   $host,
+   $authHost
+ }
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Card,
@@ -9,10 +9,30 @@ import {
 } from "react-bootstrap";
 import { REGISTRATION_ROUTE, LOGIN_ROUTE } from "../utils/consts";
 import { NavLink, useLocation } from "react-router-dom";
+import { registration } from "../http/userAPI";
+import { login } from "../http/userAPI";
+
 
 const Auth = () => {
   const location = useLocation();
   const isLogin = location.pathname === LOGIN_ROUTE;
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  
+  const click = async () => {
+         if (isLogin) {
+          const response = await login()
+         } else {
+            const response = await registration(email, password)
+            console.log(response)
+         }
+        
+        
+  }
+
+
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -22,8 +42,19 @@ const Auth = () => {
         <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
 
         <Form className="d-flex flex-column">
-          <Form.Control className="mt-3" placeholder="Введите ваш email" />
-          <Form.Control className="mt-3" placeholder="Введите ваш пароль" />
+          <Form.Control 
+          className="mt-3" 
+          placeholder="Введите ваш email"
+           value={email}
+            onChange={e => setEmail(e.target.value)}
+           />
+          <Form.Control
+          value={password} 
+          className="mt-3" 
+          placeholder="Введите ваш пароль" 
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          />
           <Row className="d-flex justify-content-between mt-3 flex-column pl-4">
             {isLogin ? (
               <div>
@@ -37,7 +68,10 @@ const Auth = () => {
               </div>
             )}
 
-            <Button variant={"outline-sucess"} className="mt-3 align-self-end">
+            <Button 
+            onClick={click}
+            variant={"outline-success"} 
+            className="mt-3 align-self-end">
               {isLogin ? "Войти" : "Регистрация"}
             </Button>
           </Row>
