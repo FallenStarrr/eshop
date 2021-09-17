aa4d2a3c3685cf31fb845e3ffb70ace2e25d89d3
Fallen Starrr <juzopigeon@gmail.com>
2021-09-17 18:51:50 +0600

    change

---
+++ client/src/http/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+
+const $host = axios.create({
+  baseUrl: ''
+})
+
+
+const $authHost = axios.create({
+  
+})
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import UserStore from './store/UserStore'
 import DeviceStore from './store/DeviceStore'
 
  export const  Context = createContext(null)
-
+ console.log(process.env.REACT_APP_API_URL)
 
 ReactDOM.render(
 
